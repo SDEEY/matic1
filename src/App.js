8e2aa27a5b36e414f8db79ff39cb70e3807b3035
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-07-28 00:43:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 import imgDiscord from "./icons8-discord-50.png";
 import imgTwitter from "./icons8-twitter-50.png";
 
-const ethAmount = "0";
-const image = "https://pbs.twimg.com/profile_images/1681974918844665858/WBTc_Jm9_400x400.jpg";
-const Title = "BYTE CITY";
-const supply = 2971;
+const ethAmount = "25/30";
+const image = "https://pbs.twimg.com/media/F2CUL2RXgAMxJ3x?format=jpg&name=large";
+const Title = "𝕊𝕡𝕒𝕔𝕖 𝕊𝕜𝕖𝕝𝕝𝕚𝕖𝕤";
+const supply = 971;
 
 document.title = Title;
 document.getElementById("favicon").setAttribute("href", image);
